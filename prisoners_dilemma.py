b6b34123fa32a458cd91d470f0e70f1f7f17d453
DaW <dwur1901@education.mpls.k12.mn.us>
2018-03-15 08:14:29 -0500

    .

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -1,6 +1,6 @@
 team_name = "DaW"
-strategy_name = "Tit for tat, with cheats."
-strategy_description = "Returns opponents last move, whilst adding to score."
+strategy_name = "Tit for tat"
+strategy_description = "Returns opponents last move, whilst hopefully adding to score."
 def move(my_history, their_history, my_score, their_score):
     my_score += 1
     if len(my_history) == 0:
